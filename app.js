1a408c87ad0218ddc5b369aeba6a5ac7820d5b5d
smithapatil12 <77383320+smithapatil12@users.noreply.github.com>
2021-02-03 11:46:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var http =require ('http');
-var Port = process.env.Port || 3000;
+var PORT = process.env.port || 3000;
 var server = http.createServer(function (req,res) {
     res.end('server works by smitha patil !!!');
 });
-server.listen(Port,'localhost',function () {
+server.listen(port,'localhost',function () {
     console.log('server started on the port:3000');
 });
